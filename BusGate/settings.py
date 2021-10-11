deef43e12674325e883d13c2cbff8e3b5ae96dc7
ProtoBytez <92336128+ProtoBytez@users.noreply.github.com>
2021-10-12 04:49:11 +0630

    ss

--- BusGate/settings.py
+++ BusGate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kq%70q-ip6ok0x^=39!bx%v@w+c(3gi+ora6&q82kj)+byqfwa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1' , 'busgatefinder.herokuapp.com']
 
