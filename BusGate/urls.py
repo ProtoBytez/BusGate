dfd7b4069e171576716bbd7ddbbb08a5276c3dd5
ProtoBytez <92336128+ProtoBytez@users.noreply.github.com>
2021-10-12 03:55:33 +0630

    d

--- BusGate/urls.py
+++ BusGate/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import url
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('Gate.urls')),
+    path('/' , include('Gate.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
